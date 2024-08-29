432f571a4fd3998affbfb7549263576f95d3061c
Chuks175 <84939923+Chuks175@users.noreply.github.com>
2024-08-29 13:49:17 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int LA[] = {1,3,5,6,7,9};
+    int ITEM = 10, k = 4, n = 6;
+    int i = 0, j = n;
+    printf("Initial Array From Start: \n");
+
+    while( i = 0){
+        i <= n;
+        i++;
+        printf("LA[%d] = %d \n", i, LA[i]);
+        }
+
+    n = n + 1;
+    n = j;
+
+    for{ j = 0; j>=n; j++; }
+        do(LA[k] = ITEM);
+
+            while( k <= j );
+
+    printf("LA[%d] = %d \n", k, LA[ITEM]);
+
+    return 0;
+}
